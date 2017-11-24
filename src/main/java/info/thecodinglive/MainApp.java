b56ea9b42f671574fff250981dc7c2537c2d6c09
sjyun <darkandark90@gmail.com>
2017-11-24 21:13:12 +0900

    init

---
+++ src/main/java/info/thecodinglive/MainApp.java
@@ -0,0 +1,11 @@
+package info.thecodinglive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+    public static void main(String ar[]){
+        SpringApplication.run(MainApp.class);
+    }
+}
